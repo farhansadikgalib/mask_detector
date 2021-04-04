26baa0e5df0facc611e0ae95191dc42f51056b8a
farhan <farhansadikgalib@gmail.com>
2021-04-04 09:17:19 +0600

    Initial commit

---
+++ android/app/src/main/kotlin/com/farhan/mask_detector/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.farhan.mask_detector
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class Splash_Screen extends StatefulWidget {
+  @override
+  _Splash_ScreenState createState() => _Splash_ScreenState();
+}
+
+class _Splash_ScreenState extends State<Splash_Screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
